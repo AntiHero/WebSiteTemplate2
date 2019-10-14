03ad1d5df311f72b86e715099110e2ed12a7e367
AntiHero <prophreaker@gmail.com>
2019-10-14 14:01:00 +0300

    feat: animation to advantages section add

--- js/main.js
+++ js/main.js
@@ -1,10 +1,46 @@
-$(window).on('load', () => {
-  $('.slider__inner').slick({
+$(window).on("load", () => {
+  $(".slider__inner").slick({
     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-    infinite: false,
-  })
+    infinite: false
+  });
 
+  $("select").styler();
 
-  $('select').styler();
+  advantagesAnim();
 });
+
+const advantagesAnim = () => {
+  /* Timeline */
+  let tl = new TimelineMax({ onUpdate: updatePercentage, yoyo: true });
+
+  /* First step */
+
+  let tween1 = TweenMax.staggerTo(
+    ".advantages-item",
+    1,
+    {
+      opacity: 1,
+      ease: Power4.easeIn
+    },
+    0.2
+  );
+
+  tl.insert(tween1);
+
+  /* Controller */
+  const controller = new ScrollMagic.Controller();
+
+  /* Scene with params */
+  const scene = new ScrollMagic.Scene({
+    triggerElement: ".advantages",
+    triggerHook: 1,
+    duration: 0
+  }).setTween(tl);
+
+  function updatePercentage() {
+    tl.progress();
+  }
+
+  scene.addTo(controller);
+};
