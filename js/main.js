302d54eb830eb70dec6c3a64d2aae14cb651adac
AntiHero <prophreaker@gmail.com>
2019-10-16 22:47:57 +0300

    feat: mobile version add

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ const advantagesAnim = () => {
   /* Timeline */
   let tl = new TimelineMax({ onUpdate: updatePercentage, yoyo: true });
 
-  /* First step */
-
   let tween1 = TweenMax.staggerTo(
     ".advantages-item",
     1,
@@ -40,6 +38,43 @@ const advantagesAnim = () => {
   });
 };
 
+const servicesAnim = () => {
+  /* Timeline */
+  let tl = new TimelineMax({ onUpdate: updatePercentage, yoyo: true });
+
+  let tween1 = TweenMax.staggerFromTo(
+    ".services__item",
+    1,
+    {opacity: .4}, {opacity: 1},
+  );
+
+  tl.insert(tween1);
+
+  /* Controller */
+  const controller = new ScrollMagic.Controller();
+
+  /* Scene with params */
+  const scene = new ScrollMagic.Scene({
+    triggerElement: ".services__items",
+    triggerHook: .8,
+    duration: 0,
+  })
+    .setTween(tl)
+    .addTo(controller);
+
+  function updatePercentage() {
+    tl.progress();
+  }
+
+  scene.on("enter", function() {
+    tl.timeScale(1);
+  });
+
+  scene.on("leave", function() {
+    tl.timeScale(3);
+  });
+}
+
 $(window).on("load", () => {
   $(".slider__inner, .news__slider-inner").slick({
     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
@@ -58,5 +93,12 @@ $(window).on("load", () => {
 
   $("select").styler();
 
+  $('.header__btn-menu').on('click', function() {
+    $('.menu ul').slideToggle();
+    
+  });
+
   advantagesAnim();
+
+  servicesAnim();
 });
